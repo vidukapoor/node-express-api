61423002e80ff6618c9de173cf1bb54beae2262e
vidukapoor <vishwadeepkapoor@gmail.com>
2019-11-15 17:01:55 +0530

    :tada: first commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# Repo for CRUD, Express and MongoDB tutorial
+
+- [Demo](https://www.herokuapp.com)
+
+## Installation
+
+1. Clone repo
+2. run `npm install` 
+
+## Usage 
+
+1. run `npm run dev`
+2. Navigate to `localhost:3000`
+3. Have fun ;)
---
+++ server.js
@@ -0,0 +1,42 @@
+const express = require('express')
+const app = express()
+const bodyParser = require('body-parser')
+const MongoClient = require('mongodb').MongoClient
+
+// const db_auth = `mongodb://<username>:<password>@ds247670.mlab.com:47670/test_analytics`;
+const db_auth = `mongodb://localhost:27017/test_analytics`;
+var db
+
+// Remember to change YOUR_USERNAME and YOUR_PASSWORD to your username and password! 
+// MongoClient.connect('mongodb://YOUR_USERNAME:YOUR_PASSWORD@ds047955.mongolab.com:47955/star-wars-quotes', (err, database) => {
+MongoClient.connect(db_auth, (err, database) => {
+  if (err) return console.log(err)
+  db = database.db('test_analytics')
+  app.listen(process.env.PORT || 3000, () => {
+    console.log('listening on 3000')
+  })
+})
+
+app.set('view engine', 'ejs')
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json())
+app.use(express.static('public'))
+
+app.get('/', (req, res) => {
+  res.render('index.ejs');
+})
+
+app.post('/create', (req, res) => {
+  db.collection('analytics').save(req.body, (err, result) => {
+    if (err) return res.json({ success: false, err });
+    console.log('saved to database')
+    res.json({ success: true });
+  })
+})
+
+app.get('/fetch', (req, res) => {
+  db.collection('analytics').find({},{sort:{_id:-1}}).toArray((err, result) => {
+    if (err) return res.json({ success: false, err });
+    res.json({ success: true, result });
+  })
+})
