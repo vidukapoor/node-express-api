1848ee788999ee03776dadcaab3ae51fca2d40f0
vidukapoor <vishwadeepkapoor@gmail.com>
2019-11-15 18:27:45 +0530

    :construction_worker: deployment fixes

--- server.js
+++ server.js
@@ -3,8 +3,10 @@ const app = express()
 const bodyParser = require('body-parser')
 const MongoClient = require('mongodb').MongoClient
 
-// const db_auth = `mongodb://<username>:<password>@ds247670.mlab.com:47670/test_analytics`;
-const db_auth = `mongodb://localhost:27017/test_analytics`;
+let db_auth = `mongodb://localhost:27017/test_analytics`;
+if (process.env.MONGO_USER) {
+  db_auth = `mongodb://${process.env.MONGO_USER}:${process.env.MONGO_PWD}@ds247670.mlab.com:47670/test_analytics`;
+}
 var db
 
 // Remember to change YOUR_USERNAME and YOUR_PASSWORD to your username and password! 
